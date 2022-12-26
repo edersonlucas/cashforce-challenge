cb2848547dee13c20582e861759631d3b3b23839
Ederson Lucas <edersonlucas@outlook.com.br>
2022-12-26 16:42:13 -0300

    feat(backend): Create Provider service

---
+++ backend/src/services/ProviderService.ts
@@ -0,0 +1,20 @@
+import Cnpj from "../database/models/Cnpj";
+import Provider from "../database/models/Provider";
+
+export default class ProviderService {
+  private model = Provider;
+
+  public async getById(id: number) {
+    const provider = await this.model.findAll({
+      where: {
+        id,
+      },
+      include: [
+        { model: Cnpj, as: 'cnpj', attributes: {
+          exclude: ['id']
+        } },
+      ],
+    });
+    return provider;
+  }
+}
