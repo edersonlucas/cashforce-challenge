ecfe336e5ff46c3b1dfe87bd31ccda0a7daf4b41
Ederson Lucas <edersonlucas@outlook.com.br>
2022-12-26 18:19:18 -0300

    test(backend): Create provider mock

---
+++ backend/src/__tests__/mocks/provider.mock.ts
@@ -0,0 +1,38 @@
+export const findByIdMock = [
+  {
+    "id": 1,
+    "name": "CEDENTE 002",
+    "tradingName": "CEDENTE 002 LTDA",
+    "cashforceTax": null,
+    "responsibleName": null,
+    "responsibleEmail": null,
+    "responsiblePosition": null,
+    "responsiblePhone": null,
+    "responsibleMobile": null,
+    "website": null,
+    "postalCode": null,
+    "address": null,
+    "number": null,
+    "complement": null,
+    "neighborhood": null,
+    "city": null,
+    "state": null,
+    "bank": null,
+    "bankAgency": null,
+    "account": null,
+    "documents": null,
+    "phoneNumber": null,
+    "situation": null,
+    "situationDate": null,
+    "createdAt": "2020-10-29T21:22:21.000Z",
+    "updatedAt": "2020-10-29T21:22:22.000Z",
+    "cnpjId": 2,
+    "email": null,
+    "cnpj": {
+      "cnpj": "00000000000002",
+      "companyType": "1",
+      "createdAt": "2020-10-29T21:20:33.000Z",
+      "updatedAt": "2020-10-29T21:20:33.000Z"
+    }
+  }
+];
