91aa61229fade510a9dfb4711bcb22bc16f997a5
Ederson Lucas <edersonlucas@outlook.com.br>
2022-12-26 08:07:40 -0300

    feat(backend): Create User seeder

---
+++ backend/src/database/seeders/20221224132406-users.js
@@ -0,0 +1,24 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, _Sequelize) {
+    await queryInterface.bulkInsert('users', [
+      {
+        id: 1,
+        name: 'ALLAN SOUZA',
+        email: 'allan@cashforce.com.br',
+        phoneNumber: null,
+        mobile: null,
+        departament: null,
+        verificationCode: '',
+        emailChecked: 1,
+        createdAt: '2020-10-01 21:31:37',
+        updatedAt: '2020-10-01 22:41:23',
+        cashforceAdm: 1,
+      },
+    ], {});
+  },
+
+  async down(queryInterface, _Sequelize) {
+    await queryInterface.bulkDelete('users', null, {});
+  },
+};
