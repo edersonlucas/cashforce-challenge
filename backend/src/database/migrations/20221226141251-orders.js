870d5251a30e1f0d763fb9bae677bc47a568ac9b
Ederson Lucas <edersonlucas@outlook.com.br>
2022-12-26 14:16:07 -0300

    feat(backend): Create Order migration

---
+++ backend/src/database/migrations/20221226141251-orders.js
@@ -0,0 +1,126 @@
+/* eslint max-lines-per-function: ["error", {"max": 200, "skipComments": true}]  */
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable(
+      'orders',
+      {
+        id: {
+          type: Sequelize.INTEGER(11),
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        orderNfId: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        orderNumber: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        orderPath: {
+          type: Sequelize.STRING,
+          allowNull: true,
+        },
+        orderFileName: {
+          type: Sequelize.STRING,
+          allowNull: true,
+        },
+        orderOriginalName: {
+          type: Sequelize.STRING,
+          allowNull: true,
+        },
+
+        emissionDate: {
+          type: Sequelize.STRING,
+          allowNull: true,
+        },
+        pdfFile: {
+          type: Sequelize.STRING,
+          allowNull: true,
+        },
+        emitedTo: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        nNf: {
+          type: Sequelize.STRING,
+          allowNull: true,
+        },
+        CTE: {
+          type: Sequelize.STRING,
+          allowNull: true,
+        },
+        value: {
+          type: Sequelize.STRING,
+          allowNull: true,
+        },
+        createdAt: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
+        updatedAt: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
+        cnpjId: {
+          type: Sequelize.INTEGER(11),
+          allowNull: true,
+          references: {
+            model: 'cnpjs',
+            key: 'id',
+          },
+        },
+        userId: {
+          type: Sequelize.INTEGER(11),
+          allowNull: true,
+          references: {
+            model: 'users',
+            key: 'id',
+          },
+        },
+        buyerId: {
+          type: Sequelize.INTEGER(11),
+          allowNull: true,
+          references: {
+            model: 'buyers',
+            key: 'id',
+          },
+        },
+        providerId: {
+          type: Sequelize.INTEGER(11),
+          allowNull: true,
+          references: {
+            model: 'providers',
+            key: 'id',
+          },
+        },
+        orderStatusBuyer: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        orderStatusProvider: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        deliveryReceipt: {
+          type: Sequelize.STRING,
+          allowNull: true,
+        },
+        cargoPackingList: {
+          type: Sequelize.STRING,
+          allowNull: true,
+        },
+        deliveryCtrc: {
+          type: Sequelize.STRING,
+          allowNull: true,
+        },
+      },
+    );
+  },
+
+  async down(queryInterface, _Sequelize) {
+    await queryInterface.dropTable('orders');
+  },
+};
