12d0323f143b944618bb8d96ce9cb99ccae399e5
Ederson Lucas <edersonlucas@outlook.com.br>
2022-12-26 16:40:16 -0300

    feat(backend): Create Error Middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import ErrorMiddleware from './middlewares/ErrorMiddlware';
 import orderRouter from './routes/OrderRouter';
 
 const app = express();
 
 app.use(express.json());
 app.use(orderRouter);
+app.use(ErrorMiddleware.error)
 
 export default app;
