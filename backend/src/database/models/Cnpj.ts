12810621df7265a7f491a004a32352d2b52daf8e
Ederson Lucas <edersonlucas@outlook.com.br>
2022-12-26 08:08:01 -0300

    feat(backend): Create a Cnpj model

---
+++ backend/src/database/models/Cnpj.ts
@@ -0,0 +1,43 @@
+import { STRING, INTEGER, DATE, Model } from 'sequelize';
+import db from '.';
+
+export default class Cnpj extends Model {
+  id!: number;
+  cnpj!: string;
+  companyType!: string;
+  createdAt!: Date;
+  updatedAt!: Date;
+};
+
+Cnpj.init(
+  {
+    id: {
+      type: INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    cnpj: {
+      type: STRING,
+      allowNull: false,
+    },
+    companyType: {
+      type: STRING,
+      allowNull: false,
+    },
+    createdAt: {
+      type: DATE,
+      allowNull: false,
+    },
+    updatedAt: {
+      type: DATE,
+      allowNull: false,
+    }
+  },
+  {
+    underscored: true,
+    sequelize: db,
+    modelName: 'Cnpjs',
+    timestamps: true,
+  },
+);
