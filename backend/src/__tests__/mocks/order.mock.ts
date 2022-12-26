6581f107f01e9076c62d5285a0032a8d3e60d776
Ederson Lucas <edersonlucas@outlook.com.br>
2022-12-26 18:18:35 -0300

    test(backend): Create order mock

---
+++ backend/src/__tests__/mocks/order.mock.ts
@@ -0,0 +1,143 @@
+export const findAllByUserIdMock = [
+  {
+    "id": 1,
+    "orderNfId": "1605181324132",
+    "orderNumber": "18153",
+    "orderPath": null,
+    "orderFileName": null,
+    "orderOriginalName": null,
+    "emissionDate": "2020-10-30T11:00:00-03:00",
+    "pdfFile": null,
+    "emitedTo": "22843980000127",
+    "nNf": "18153",
+    "CTE": "",
+    "value": "198450",
+    "createdAt": "2020-10-30T17:54:18.000Z",
+    "updatedAt": "2020-10-30T17:54:18.000Z",
+    "orderStatusBuyer": "0",
+    "orderStatusProvider": "0",
+    "deliveryReceipt": null,
+    "cargoPackingList": null,
+    "deliveryCtrc": null,
+    "provider": {
+      "id": 1,
+      "name": "CEDENTE 002"
+    },
+    "buyer": {
+      "id": 1,
+      "name": "SACADO 001"
+    },
+    "cnpj": {
+      "cnpj": "00000000000001",
+      "companyType": "2",
+      "createdAt": "2020-10-29T21:20:33.000Z",
+      "updatedAt": "2020-10-29T21:20:33.000Z"
+    },
+    "user": {
+      "name": "ALLAN SOUZA",
+      "email": "allan@cashforce.com.br",
+      "phoneNumber": null,
+      "mobile": null,
+      "departament": null,
+      "verificationCode": "",
+      "emailChecked": 1,
+      "createdAt": "2020-10-01T21:31:37.000Z",
+      "updatedAt": "2020-10-01T22:41:23.000Z",
+      "cashforceAdm": 1
+    }
+  },
+  {
+    "id": 2,
+    "orderNfId": "160518132413",
+    "orderNumber": "18157",
+    "orderPath": null,
+    "orderFileName": null,
+    "orderOriginalName": null,
+    "emissionDate": "2020-11-04T15:32:35-02:00",
+    "pdfFile": null,
+    "emitedTo": "35705180000272",
+    "nNf": "18157",
+    "CTE": "",
+    "value": "168850",
+    "createdAt": "2020-11-10T18:33:46.000Z",
+    "updatedAt": "2020-11-10T18:33:46.000Z",
+    "orderStatusBuyer": "0",
+    "orderStatusProvider": "0",
+    "deliveryReceipt": null,
+    "cargoPackingList": null,
+    "deliveryCtrc": null,
+    "provider": {
+      "id": 1,
+      "name": "CEDENTE 002"
+    },
+    "buyer": {
+      "id": 1,
+      "name": "SACADO 001"
+    },
+    "cnpj": {
+      "cnpj": "00000000000001",
+      "companyType": "2",
+      "createdAt": "2020-10-29T21:20:33.000Z",
+      "updatedAt": "2020-10-29T21:20:33.000Z"
+    },
+    "user": {
+      "name": "ALLAN SOUZA",
+      "email": "allan@cashforce.com.br",
+      "phoneNumber": null,
+      "mobile": null,
+      "departament": null,
+      "verificationCode": "",
+      "emailChecked": 1,
+      "createdAt": "2020-10-01T21:31:37.000Z",
+      "updatedAt": "2020-10-01T22:41:23.000Z",
+      "cashforceAdm": 1
+    }
+  },
+  {
+    "id": 3,
+    "orderNfId": "1605181324130",
+    "orderNumber": "18184",
+    "orderPath": null,
+    "orderFileName": null,
+    "orderOriginalName": null,
+    "emissionDate": "2020-11-10",
+    "pdfFile": null,
+    "emitedTo": "00418477002640",
+    "nNf": "18184",
+    "CTE": "",
+    "value": "222795",
+    "createdAt": "2020-11-12T11:42:06.000Z",
+    "updatedAt": "2020-11-18T12:22:14.000Z",
+    "orderStatusBuyer": "0",
+    "orderStatusProvider": "0",
+    "deliveryReceipt": null,
+    "cargoPackingList": null,
+    "deliveryCtrc": null,
+    "provider": {
+      "id": 1,
+      "name": "CEDENTE 002"
+    },
+    "buyer": {
+      "id": 1,
+      "name": "SACADO 001"
+    },
+    "cnpj": {
+      "cnpj": "00000000000001",
+      "companyType": "2",
+      "createdAt": "2020-10-29T21:20:33.000Z",
+      "updatedAt": "2020-10-29T21:20:33.000Z"
+    },
+    "user": {
+      "name": "ALLAN SOUZA",
+      "email": "allan@cashforce.com.br",
+      "phoneNumber": null,
+      "mobile": null,
+      "departament": null,
+      "verificationCode": "",
+      "emailChecked": 1,
+      "createdAt": "2020-10-01T21:31:37.000Z",
+      "updatedAt": "2020-10-01T22:41:23.000Z",
+      "cashforceAdm": 1
+    }
+  }
+];
