39d1aa92a3ca31c24e865acf5fc85e5168cade9c
Ederson Lucas <edersonlucas@outlook.com.br>
2023-01-03 18:03:40 -0300

    feat(backend): Create auth middleware

---
+++ backend/src/middlewares/AuthMiddleware.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from 'express';
+import { JwtPayload } from 'jsonwebtoken';
+import ErrorGenerator from '../utils/ErrorGenerator';
+import Auth from '../utils/Auth';
+
+interface RequestAuth extends Request {
+  user?: JwtPayload | string,
+}
+
+export default class AuthMiddleware {
+  public static auth(req: RequestAuth, _res: Response, next: NextFunction) {
+    const token = req.headers.authorization;
+    if (!token) throw new ErrorGenerator(401, 'Token not found');
+    try {
+      const user = new Auth().Authorization(token);
+      req.user = user;
+      next();
+    } catch (_err) {
+      throw new ErrorGenerator(401, 'Token must be a valid token');
+    }
+  }
+}
