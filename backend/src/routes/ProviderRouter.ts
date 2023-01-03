894a01ce7b818f443c20be4f2fefa6dedd828df4
Ederson Lucas <edersonlucas@outlook.com.br>
2023-01-03 18:06:21 -0300

    refactor(backend): Provider router

--- backend/src/routes/ProviderRouter.ts
+++ backend/src/routes/ProviderRouter.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import ProviderController from '../controllers/ProviderController';
+import AuthMiddleware from '../middlewares/AuthMiddleware';
 
 const providerRouter = Router();
 
 providerRouter
-  .get('/provider/:id', new ProviderController().getById);
+  .get('/provider/:id', AuthMiddleware.auth, new ProviderController().getById);
 
 export default providerRouter;
