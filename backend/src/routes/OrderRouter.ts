a4f5c53c289d4d641c924e0e3b51a6480be5d049
Ederson Lucas <edersonlucas@outlook.com.br>
2023-01-03 18:06:06 -0300

    refactor(backend): Order router

--- backend/src/routes/OrderRouter.ts
+++ backend/src/routes/OrderRouter.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import OrderController from '../controllers/OrderController';
+import AuthMiddleware from '../middlewares/AuthMiddleware';
 
 const orderRouter = Router();
 
 orderRouter
-  .get('/order/:id', new OrderController().getAllByUserId);
+  .get('/orders', AuthMiddleware.auth, new OrderController().getAll);
 
 export default orderRouter;
