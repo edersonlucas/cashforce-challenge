42eb8ee1e1bd7252b16d53517542bfdce3fa4cbf
Ederson Lucas <edersonlucas@outlook.com.br>
2023-01-02 22:13:35 -0300

    fix(backend): linter

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import app from './app';
 
-const PORT = Number(process.env.BACKEND_PORT);
+const PORT = Number(process.env.BACKEND_PORT) || 3001;
 
 const server = app.listen(PORT, () => console.log(
   `Server is running on PORT: ${PORT}`,
