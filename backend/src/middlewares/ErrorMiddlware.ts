4bd0e78491ea09c3d3fbe394a066fffcad46e1ca
Ederson Lucas <edersonlucas@outlook.com.br>
2023-01-03 18:02:58 -0300

    refactor(backend): Error middleware

--- backend/src/middlewares/ErrorMiddlware.ts
+++ backend/src/middlewares/ErrorMiddlware.ts
@@ -1,7 +1,10 @@
 import { Request, Response, NextFunction } from 'express';
+import IError from '../interfaces/IError';
 
 export default class ErrorMiddleware {
-  public static error(_err: Error, _req: Request, res: Response, _next: NextFunction) {
+  public static error(err: IError, _req: Request, res: Response, _next: NextFunction) {
+    const { code, message } = err;
+    if (code) return res.status(code).json({ message });
     return res.status(500).json({ message: 'Internal Server Error!' });
   }
 }
