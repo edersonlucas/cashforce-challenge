1884c46e651aa0dcae97643839d37fe5c31bd576
Ederson Lucas <edersonlucas@outlook.com.br>
2023-01-04 14:57:28 -0300

    docs(backend): Configure swagger ui

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,19 @@
 import express from 'express';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
 import ErrorMiddleware from './middlewares/ErrorMiddlware';
 import orderRouter from './routes/OrderRouter';
 import providerRouter from './routes/ProviderRouter';
 import authRouter from './routes/AuthRouter';
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const swaggerDocs = require('./swagger.json');
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use(orderRouter);
 app.use(providerRouter);
 app.use(authRouter);
