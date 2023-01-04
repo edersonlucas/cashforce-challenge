c66419d2c2167d9bb0f52546277333fe1b646ef7
Ederson Lucas <edersonlucas@outlook.com.br>
2023-01-04 06:20:33 -0300

    feat(frontend): Create the home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom'
+import AsideMenu from './components/AsideMenu';
+import Header from './components/Header';
 import Home from './pages/Home';
 import Invoice from './pages/Invoice';
 
 function App() {
   return (
+  <div className="flex flex-col min-h-screen max-w-screen bg-white-900 text-white-900 mx-auto">
+  <Header/>
+    <AsideMenu />
   <Routes>
     <Route path="/" element={ <Home/> } />
     <Route path="/invoices" element={ <Invoice/> } />
     <Route path="*" element={ <Home/> } />
   </Routes>
+  </div>
   );
 }
 
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { parseCookies } from 'nookies';
+import { useNavigate } from 'react-router-dom';
+import CashforceTeam from '../assets/images/CashforceTeam.webp'
 
 export default function Home () {
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const { 'cashforce.token': token } = parseCookies();
+    if (!token) {
+      navigate('/login');
+    }
+  }, [navigate]);
+
   return (
-    <p>Home</p>
+    <div className="mt-[75px] p-2 max-w-full lg:w-[calc(100%-250px)] flex-col lg:flex-row lg:ml-[250px] grow p-none flex items-center justify-center mx-auto">
+      <div>
+        <h1 className="text-blue-700 max-w-[500px] text-5xl">A <span className="text-green-500">evolução</span> do Supply Chain Finance.</h1>
+        <p className="text-blue-300 max-w-[550px]">Enxergando a cadeia produtiva pelo lado financeiro, as oportunidades se multiplicam.</p>
+      </div>
+      <img
+        src={CashforceTeam}
+        alt="Team cashforce"
+        className='w-2/5 hidden lg:block'
+      />
+    </div>
   )
 }
